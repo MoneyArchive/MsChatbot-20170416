2254ea8c0fc44dbe27d996b9d7a286f1d1437b08
Money <abc12207@gmail.com>
2017-04-16 14:10:31 +0800

    1410

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -27,24 +27,39 @@ namespace Bot_Application1.Dialogs
             var openJson = File.ReadAllText(HttpContext.Current.Server.MapPath("~/App_Data/YouBikeTP.json"));
             List<Models.UbikeStation> stations = JsonConvert.DeserializeObject<List<Models.UbikeStation>>(openJson);
 
-            var data = stations.FirstOrDefault(x => x.sna.Contains(activity.Text));
-            if (data == null)
+            var datas = stations.Where(x => x.sna.Contains(activity.Text)).ToList();
+            if (!datas.Any())
             {
                 await context.PostAsync($"搜尋不到相關ubike站點資訊");
             }
             else
             {
-                var hero = new HeroCard();
-                hero.Title = data.sna;
-                hero.Subtitle = data.ar;
-                hero.Images = new List<CardImage>();
-                hero.Images.Add(new CardImage(){Url = $"http://maps.google.com/maps/api/staticmap?center={data.lat},{data.lng}&zoom=16&size=400x400&sensor=false&format=png32&maptype=roadmap&markers={data.lat},{data.lng}" });
-
                 var reply = context.MakeMessage();
-                reply.Type = "message";
+                reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
                 reply.Attachments = new List<Attachment>();
-                reply.Attachments.Add(hero.ToAttachment());
 
+                foreach (Models.UbikeStation data in datas)
+                {
+
+                    var hero = new HeroCard();
+                    hero.Title = data.sna;
+                    hero.Subtitle = data.ar;
+                    hero.Images = new List<CardImage>();
+                    hero.Images.Add(new CardImage()
+                    {
+                        Url =
+                            $"http://maps.google.com/maps/api/staticmap?center={data.lat},{data.lng}&zoom=16&size=400x400&sensor=false&format=png32&maptype=roadmap&markers={data.lat},{data.lng}"
+                    });
+                    hero.Buttons = new List<CardAction>();
+                    hero.Buttons.Add(new CardAction()
+                    {
+                        Title = "用地圖開啟",
+                        Type = ActionTypes.OpenUrl,
+                        Value = $"http://maps.google.com/maps?z=12&t=m&q=loc:{data.lat}+{data.lng}"
+                    });
+
+                    reply.Attachments.Add(hero.ToAttachment());
+                }
                 await context.PostAsync(reply);
             }
 
