6372aeb4c060d11ef01faed6c3b8667f0c7a5e44
Money <abc12207@gmail.com>
2017-04-16 15:23:08 +0800

    1523

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Microsoft.ProjectOxford.Vision;
 using Newtonsoft.Json;
 
 namespace Bot_Application1.Dialogs
@@ -24,23 +25,25 @@ namespace Bot_Application1.Dialogs
         {
             var activity = await result as Activity;
 
-            var openJson = File.ReadAllText(HttpContext.Current.Server.MapPath("~/App_Data/YouBikeTP.json"));
-            List<Models.UbikeStation> stations = JsonConvert.DeserializeObject<List<Models.UbikeStation>>(openJson);
-
-            var datas = stations.Where(x => x.sna.Contains(activity.Text)).ToList();
-            if (!datas.Any())
+            if (activity.Attachments.Any() && activity.Attachments.First().ContentType.StartsWith("image"))
             {
-                await context.PostAsync($"搜尋不到相關ubike站點資訊");
+                VisionServiceClient client = new VisionServiceClient("809703eae3894ddf9e2e9b9d33b6cab1");
+                string url = activity.Attachments.First().ContentUrl;
+                var output = await client.AnalyzeImageAsync(url, new VisualFeature[] {VisualFeature.Description});
+                await context.PostAsync(output.Description.Captions.First().Text);
             }
             else
             {
-                var reply = context.MakeMessage();
-                reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
-                reply.Attachments = new List<Attachment>();
+                var openJson = File.ReadAllText(HttpContext.Current.Server.MapPath("~/App_Data/YouBikeTP.json"));
+                List<Models.UbikeStation> stations = JsonConvert.DeserializeObject<List<Models.UbikeStation>>(openJson);
 
-                foreach (Models.UbikeStation data in datas)
+                var data = stations.FirstOrDefault(x => x.sna.Contains(activity.Text));
+                if (data == null)
                 {
-
+                    await context.PostAsync($"搜尋不到相關ubike站點資訊");
+                }
+                else
+                {
                     var hero = new HeroCard();
                     hero.Title = data.sna;
                     hero.Subtitle = data.ar;
@@ -48,19 +51,23 @@ namespace Bot_Application1.Dialogs
                     hero.Images.Add(new CardImage()
                     {
                         Url =
-                            $"http://maps.google.com/maps/api/staticmap?center={data.lat},{data.lng}&zoom=16&size=400x400&sensor=false&format=png32&maptype=roadmap&markers={data.lat},{data.lng}"
+                            $"https://maps.google.com/maps/api/staticmap?center={data.lat},{data.lng}&zoom=16&size=400x400&sensor=false&format=png32&maptype=roadmap&markers={data.lat},{data.lng}"
                     });
                     hero.Buttons = new List<CardAction>();
                     hero.Buttons.Add(new CardAction()
                     {
                         Title = "用地圖開啟",
                         Type = ActionTypes.OpenUrl,
-                        Value = $"http://maps.google.com/maps?z=12&t=m&q=loc:{data.lat}+{data.lng}"
+                        Value = $"https://maps.google.com/maps?z=12&t=m&q=loc:{data.lat}+{data.lng}"
                     });
 
+                    var reply = context.MakeMessage();
+                    reply.Type = "message";
+                    reply.Attachments = new List<Attachment>();
                     reply.Attachments.Add(hero.ToAttachment());
+
+                    await context.PostAsync(reply);
                 }
-                await context.PostAsync(reply);
             }
 
             context.Wait(MessageReceivedAsync);
