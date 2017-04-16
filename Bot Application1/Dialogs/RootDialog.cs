bbfae9e2593f241614faed038309980b65f7daac
Money <abc12207@gmail.com>
2017-04-16 11:31:41 +0800

    1131

---
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            var openJson = File.ReadAllText(HttpContext.Current.Server.MapPath("~/App_Data/YouBikeTP.json"));
+            List<Models.UbikeStation> stations = JsonConvert.DeserializeObject<List<Models.UbikeStation>>(openJson);
+
+            var data = stations.FirstOrDefault(x => x.sna.Contains(activity.Text));
+            if (data == null)
+            {
+                await context.PostAsync($"搜尋不到相關ubike站點資訊");
+            }
+            else
+            {
+                var hero = new HeroCard();
+                hero.Title = data.sna;
+                hero.Subtitle = data.ar;
+                hero.Images = new List<CardImage>();
+                hero.Images.Add(new CardImage(){Url = $"http://maps.google.com/maps/api/staticmap?center={data.lat},{data.lng}&zoom=16&size=400x400&sensor=false&format=png32&maptype=roadmap&markers={data.lat},{data.lng}" });
+
+                var reply = context.MakeMessage();
+                reply.Type = "message";
+                reply.Attachments = new List<Attachment>();
+                reply.Attachments.Add(hero.ToAttachment());
+
+                await context.PostAsync(reply);
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Bot Application1/Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1
+{
+    public class Models
+    {
+        public class UbikeStation
+        {
+            public string sno { get; set; }
+            public string sna { get; set; }
+            public string tot { get; set; }
+            public string sbi { get; set; }
+            public string sarea { get; set; }
+            public string mday { get; set; }
+            public string lat { get; set; }
+            public string lng { get; set; }
+            public string ar { get; set; }
+            public string sareaen { get; set; }
+            public string snaen { get; set; }
+            public string aren { get; set; }
+            public string bemp { get; set; }
+            public string act { get; set; }
+        }
+    }
+}
